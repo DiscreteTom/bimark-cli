a13bc4988bf801aec53dece50b6f2c56a60ce4f5
DiscreteTom <discrete_tom@outlook.com>
2023-03-23 10:29:01 +0800

    feat: add biml support

---
+++ src/biml.ts
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+
+import { BiML } from "bimark";
+import yargs from "yargs";
+import {
+  statSync,
+  readFileSync,
+  readdirSync,
+  writeFileSync,
+  mkdirSync,
+} from "fs";
+
+const argv = yargs(process.argv.slice(2))
+  .usage("Usage: biml [options] <file|folder> [file|folder]...")
+  .options({
+    da: {
+      alias: "def-alias",
+      type: "boolean",
+      description: "Show definition alias",
+      default: false,
+    },
+    db: {
+      alias: "def-bracket",
+      type: "boolean",
+      description: "Show definition bracket",
+      default: false,
+    },
+    rb: {
+      alias: "ref-bracket",
+      type: "boolean",
+      description: "Show reference bracket",
+      default: false,
+    },
+    f: {
+      alias: "filter",
+      type: "string",
+      description: "Filter by file suffix",
+      default: ".html",
+    },
+    o: {
+      alias: "output",
+      type: "string",
+      description: "Output path",
+      default: "",
+    },
+  })
+  .parseSync();
+
+const biml = new BiML();
+
+function collectFile(path: string) {
+  biml.collect(path, readFileSync(path, "utf-8"));
+}
+
+function renderFile(path: string) {
+  const res = biml.render(path, readFileSync(path, "utf-8"), {
+    def: { showAlias: argv.da, showBrackets: argv.db },
+    ref: { showBrackets: argv.rb },
+  });
+  if (argv.o) {
+    mkdirSync((argv.o + "/" + path).split("/").slice(0, -1).join("/"), {
+      recursive: true,
+    });
+    writeFileSync(argv.o + "/" + path, res);
+  } else {
+    console.log(res);
+  }
+}
+
+function traverseFolder(path: string, cb: (path: string) => void) {
+  readdirSync(path, { withFileTypes: true }).forEach((dirent) => {
+    if (dirent.isFile()) {
+      if (dirent.name.endsWith(argv.f)) {
+        cb(path + "/" + dirent.name);
+      }
+    } else if (dirent.isDirectory()) {
+      traverseFolder(path + "/" + dirent.name, cb);
+    }
+  });
+}
+
+// collect
+(argv._ as string[]).forEach((path) => {
+  if (statSync(path).isFile()) collectFile(path);
+  else traverseFolder(path, collectFile);
+});
+
+// render
+(argv._ as string[]).forEach((path) => {
+  if (statSync(path).isFile()) renderFile(path);
+  else traverseFolder(path, renderFile);
+});
